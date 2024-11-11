09c29e091aad8f5bf0cb1eb99ed42c34d6f88eee
kiswaafaq <kiswaafaque14@gmail.com>
2024-11-10 21:48:37 -0800

    Updated version

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,20 +2,20 @@ import Link from "next/link"
 
 export default function contact(){
  return(
-    <div className="flex flex-col space-y-4">
-        <h1 className="text-center font-bold text-xl bg-[#CBDCEB] h-30">Contact</h1>
-        <h1 className="p=4 m-4 text-[#4F1787]">If you are interested in hiring, then you can surely contact me on:</h1>
+    <div className="flex flex-col space-y-4 h-20 bg-gradient-to-r from-cyan-500 to-blue-500">
+        <h1 className="text-center font-bold text-xl h-30 p-6 text-[#E1F7F5]">Contact</h1>
+        <h1 className="p=4 m-4 text-[#003C43] m-3">If you are interested in hiring, then you can surely contact me on:</h1>
         <ol>
-            <li className="text-[#608BC1] font-bold bg-[#D4F6FF]"><Link href="https://www.linkedin.com/in/kiswaafaq?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"> 1 Linkedin</Link></li>
-            <li className="text-[#608BC1] font-bold bg-[#D4F6FF]"><Link href="kiswafaque14@gmail.com">2 Email</Link></li>
-            <li className="text-[#608BC1] font-bold bg-[#D4F6FF]"><Link href="https://www.instagram.com/k1swa_aa?igsh=MThkZXplbHQ5YXF6cA==">3 Instagram</Link></li>
+            <li className="text-[#609BC1] font-bold bg-[#D4F6FF] font-sans md:font-serif"><Link href="https://www.linkedin.com/in/kiswaafaq?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"> 1 Linkedin</Link></li>
+            <li className="text-[#608BC1] font-bold bg-[#D4F6FF] font-sans md:font-serif"><Link href="kiswafaque14@gmail.com">2 Email</Link></li>
+            <li className="text-[#608BC1] font-bold bg-[#D4F6FF] font-sans md:font-serif"><Link href="https://www.instagram.com/k1swa_aa?igsh=MThkZXplbHQ5YXF6cA==">3 Instagram</Link></li>
         </ol>
-        <p className="test-[#4F1787] p-4 m-4">Thanks for taking out the time to view my resume.</p>
+        <p className="test-[#4F1787] p-4 m-4">Thank you for taking the time to read my resume. I look forward to the opportunity to bring my skills and dedication to your team.</p>
         <ul className="flex space-x-10">
-    <li className="text-[#133E87] font-bold bg-[#B9E5E8]"><Link href="/">Home</Link></li>
-    <li className="text-[#133E87] font-bold bg-[#B9E5E8]"> <Link href="/about"> About </Link> </li>
-    <li className="text-[#133E87] font-bold bg-[#B9E5E8]"> <Link href="/qualification">Qualifications</Link></li>
-    <li className="text-[#133E87] font-bold bg-[#B9E5E8]"> <Link href="/contact"> Contact </Link> </li>
+    <li className="text-[#133E87] font-bold bg-[#B9E5E8] font-sans hover:font-serif m-3"><Link href="/">Home</Link></li>
+    <li className="text-[#133E87] font-bold bg-[#B9E5E8] font-sans hover:font-serif m-3"> <Link href="/about"> About </Link> </li>
+    <li className="text-[#133E87] font-bold bg-[#B9E5E8] font-sans hover:font-serif m-3"> <Link href="/qualification">Qualifications</Link></li>
+    <li className="text-[#133E87] font-bold bg-[#B9E5E8] font-sans hover:font-serif m-3"> <Link href="/contact"> Contact </Link> </li>
 
   </ul> 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,17 @@ export default function Home(){
     <div className="flex flex-col space-y-4">
        <h1 className="font-bold bg-[#DFF2EB] text-[#4A628A]  text-2xl italic text-center leading-loose ">Kiswa Afaq</h1>
        <Image src={profile} width={200} height={200} alt="Profile Picture" />
-      <p className="text-center bg-[#B9E5E8] text-[#4A628A] text-xl italic underline "> Student </p>
+      <p className=" text-[#4A628A] text-xl italic underline p-3 m-6 "> Student </p>
   <p className="text-left italic p-3 m-3">Motivated college student with a strong foundation in web development, currently working on a static resume builder project using CSS, JavaScript, TypeScript, and Tailwind CSS. 
     I am passionate about learning new technologies and continuously enhancing my skills, with an eye on creating functional, aesthetically pleasing web experiences.
      Known for my attention to detail and dedication, I am eager to bring fresh ideas and a strong work ethic to a dynamic development team. 
     Proficient in VS Code and driven to succeed in the IT field. </p>
     <ul className="flex space-x-10">
-    <li className="text-[#133E87] font-bold bg-[#B9E5E8]"><Link href="/">Home</Link></li>
-    <li className="text-[#133E87] font-bold bg-[#B9E5E8]"> <Link href="/about"> About </Link> </li>
-    <li className="text-[#133E87] font-bold bg-[#B9E5E8]"> <Link href="/qualification">Qualifications</Link></li>
-    <li className="text-[#133E87] font-bold bg-[#B9E5E8]"> <Link href="/contact"> Contact </Link> </li>
-
-  </ul> 
+    <li className="text-[#133E87] font-bold bg-[#B9E5E8] font-sans hover:font-serif m-3 "><Link href="/">Home</Link></li>
+    <li className="text-[#133E87] font-bold bg-[#B9E5E8] font-sans hover:font-serif m-3 "> <Link href="/about"> About </Link> </li>
+    <li className="text-[#133E87] font-bold bg-[#B9E5E8] font-sans hover:font-serif m-3 "> <Link href="/qualification">Qualifications</Link></li>
+    <li className="text-[#133E87] font-bold bg-[#B9E5E8] font-sans hover:font-serif m-3 "> <Link href="/contact"> Contact </Link> </li>
+ </ul> 
      </div>
   );  
 }
